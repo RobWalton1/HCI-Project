1c729b832ac597c6a6fb293d89bb09f7e5e859db
Rob Walton <robwalton06@gmail.com>
2022-12-14 15:36:38 +0000

    Updated with much better report page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {backgroundImage: {
+      'hero': "url('UEA.jpg')",
+    }
+  },
   },
   plugins: [require("daisyui")],
 }
